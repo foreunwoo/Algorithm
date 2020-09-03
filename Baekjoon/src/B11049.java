c2db22a3fb9a6764bf8746e37f0effc2e3439be2
foreunwoo <agasiya99@gmail.com>
2020-09-03 23:17:52 +0900

    백준 11049

--- Baekjoon/src/B11049.java
+++ Baekjoon/src/B11049.java
@@ -1,36 +1,31 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class B11049 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
-        int [][] c = new int[n+1][2];
-        int [][] dp = new int[n+1][n+1];
-        int [] p = new int[n+1];
+        int [][] c = new int[n][2];
+        int [][] dp = new int[n][n];
 
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<n; i++) {
             c[i][0] = sc.nextInt();
             c[i][1] = sc.nextInt();
-
-            p[i] = c[i][1];
         }
 
-        p[0] = c[1][0];
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<n-i; j++) {
+                dp[j][j+i] = Integer.MAX_VALUE;
 
-        for(int i=2; i<=n; i++) {
-            for(int j=i-1; j > 0; j--) {
-                dp[j][i] = 99999999;
-
-                for(int k=j; k <= i; k++) {
-                    dp[j][i] = Math.min(dp[j][i], c[j][k] + c[k+1][i] + (p[j-1] * p[k] * p[i]));
+                for(int k=0; k<i; k++) {
+                    dp[j][j+i] = Math.min(dp[j][j+i], dp[j][j+k] + dp[j+k+1][j+i] + c[j][0] * c[j+k][1] * c[j+i][1]);
                 }
             }
         }
 
-        for(int i=0; i<n; i++) {
-            System.out.println(p[i]);
-        }
+        System.out.println(dp[0][n-1]);
+        sc.close();
     }
 }
